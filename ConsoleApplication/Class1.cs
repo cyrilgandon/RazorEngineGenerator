09176c5eb0e0154266c80f1085caac356487a458
Cyril Gandon <cyril.gandon@res-group.com>
2016-03-01 12:07:55 +0100

    Mount up dll project an unit test project

---
+++ ConsoleApplication/Class1.cs
@@ -0,0 +1,18 @@
+﻿using RazorEngine;
+using RazorEngine.Templating;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    public class Class1
+    {
+        Class1()
+        {
+
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RazorEngine;
+using RazorEngine.Templating;
+using System.IO;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var service = Engine.Razor;
+            var template = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Views", "test.cshtml");
+            var templateFile = File.ReadAllText(template);
+            var result = service.RunCompile(templateFile, Guid.NewGuid().ToString());
+        }
+    }
+}
